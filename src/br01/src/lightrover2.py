663a7981caa92c0bedf1bad6bb1f4f1f1cfc15bf
Raku <rakutek12@gmail.com>
2022-04-15 15:18:15 +0900

    odom

--- src/br01/src/lightrover2.py
+++ src/br01/src/lightrover2.py
@@ -30,6 +30,36 @@ class LightRover():
         uR, uL = self.v_to_u(vR), self.v_to_u(vL)
         self.motor.drive(uR, uL)
 
+    def update_odom(self):
+        self.odom.update()
+        v, w = (self.odom.vR + self.odom.vL) / 2, self.odom.w
+        x, y, th = self.odom.x, self.odom.y, self.odom.theta
+        t = rospy.Time.now()
+
+        q = tf.transformations.quaternion_from_euler(0, 0, th)
+        self.bc_odom.sendTransform(
+            (x, y, 0.0), q, t, "base_link", "odom")
+
+        odom = Odometry()
+        odom.header.stamp = t
+        odom.header.frame_id = "odom"
+        odom.child_frame_id = "base_link"
+        odom.pose.pose.position = Point(x, y, 0.0)
+        odom.pose.pose.orientation = Quaternion(*q)
+        odom.twist.twist.linear.x = v
+        odom.twist.twist.angular.z = w
+        self.pub_odom.publish(odom)
+
+
+if __name__ == '__main__':
+    rospy.init_node('lightrover')
+    n = LightRover()
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        n.update_motor()
+        n.update_odom()
+        rate.sleep()
+
 
 if __name__ == '__main__':
     rospy.init_node('light_rover')
